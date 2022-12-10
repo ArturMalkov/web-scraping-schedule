fe3942b924d8662d59c50b0cfaf423c85ab52c71
Artur <artur180892@mail.ru>
2022-12-10 21:37:52 +0300

    Add ProductScrapeEvent to models with corresponding crud functionality

--- app/crud.py
+++ app/crud.py
@@ -1,12 +1,26 @@
+import uuid
+
 from cassandra.cqlengine.management import sync_table
 
 from app.db import get_session
-from app.models import Product
+from app.models import Product, ProductScrapeEvent
 
 
 session = get_session()
 sync_table(Product)  # to avoid running migrations
+sync_table(ProductScrapeEvent)
 
 
 def create_entry(data: dict):
     return Product.create(**data)
+
+
+def create_scrape_entry(data: dict):
+    data["uuid"] = uuid.uuid1()  # uuid1 includes a timestamp
+    return ProductScrapeEvent.create(**data)
+
+
+def add_scrape_event(data: dict):
+    product = create_entry(data)
+    scrape_object = create_scrape_entry(data)
+    return product, scrape_object
--- app/models.py
+++ app/models.py
@@ -2,9 +2,20 @@ from cassandra.cqlengine import columns
 from cassandra.cqlengine.models import Model
 
 
+# List View -> Detail View
 class Product(Model):
     __keyspace__ = "scraper_app"
 
     asin = columns.Text(primary_key=True, required=True)  # 'asin' stands for 'Amazon shipping id number'
+    title = columns.Text()
+    price_str = columns.Text(default="-1")
+
+
+# Detail View for asin
+class ProductScrapeEvent(Model):
+    __keyspace__ = "scraper_app"
+
+    uuid = columns.UUID(primary_key=True)
+    asin = columns.Text(index=True)
     title = columns.Text()
     price_str = columns.Text(default="-1")
