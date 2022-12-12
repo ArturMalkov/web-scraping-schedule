2f324ea1e13a81d01b031cb319821945d64d6464
Artur <artur180892@mail.ru>
2022-12-12 19:16:36 +0300

    Implement scraper scrolling functionality

--- app/scraper.py
+++ app/scraper.py
@@ -1,3 +1,6 @@
+import time
+from dataclasses import dataclass
+
 from fake_useragent import UserAgent
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -8,7 +11,11 @@ def get_user_agent():
     return UserAgent(verify_ssl=False).random
 
 
+@dataclass
 class Scraper:
+    url: str
+    endless_scroll: bool = False
+    endless_scroll_time: int = 5
     driver: WebDriver = None
 
     def get_driver(self):
@@ -24,3 +31,30 @@ class Scraper:
             self.driver = driver
 
         return self.driver
+
+    def perform_endless_scroll(self, driver=None):
+        if driver is None:
+            return
+
+        if self.endless_scroll:
+            # driver.execute_script()
+            current_height = driver.execute_script(
+                "return document.body.scrollHeight"
+            )
+            while True:
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+                time.sleep(self.endless_scroll_time)
+                iteration_height = driver.execute_script(
+                    "return document.body.scrollHeight"
+                )
+                if current_height == iteration_height:
+                    break
+
+                current_height = iteration_height
+
+    def get(self):
+        driver = self.get_driver()
+        driver.get(self.url)
+        self.perform_endless_scroll(driver=driver)
+
+        return driver.page_source
